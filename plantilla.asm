7083b72a64354b194cdca3a165ea53474eb13f93
JP <axel_jimnz5p@outlook.com>
2023-06-08 23:01:25 -0600

    plantilla2

--- plantilla.asm
+++ plantilla.asm
@@ -105,7 +105,125 @@ ends
 
 .stack segment				 
  dw   128  dup(0)			 
- dw   128  dupy(0)		 
+ dw   128  dupy(0)	
+ 
+section .text
+    global _start
+
+_start:
+    ; DAA: Decimal Adjust Accumulator
+    ; Ajusta el contenido del registro AL despu�s de una operaci�n de suma o resta en BCD.
+    ; Esta instrucci�n ajusta los d�gitos decimales y el flag de acarreo.
+    mov al, 0x49     
+    daa             
+    
+    ; DAS: Decimal Adjust for Subtraction
+    ; Ajusta el contenido del registro AL despu�s de una operaci�n de resta en BCD.
+    ; Esta instrucci�n ajusta los d�gitos decimales y el flag de acarreo.
+    mov al, 0x32     
+    das             
+
+    ; PUSHF: Push Flags onto Stack
+    ; Guarda los flags en la pila de la CPU.
+    pushf 
+
+    ; RET: Return from Procedure
+    ; Devuelve el control a la instrucci�n siguiente a la llamada de la funci�n.
+    ret           
+
+    ; CLC: Clear Carry Flag
+    ; Borra el flag de acarreo (Carry Flag).
+    clc 
+
+    ; CLD: Clear Direction Flag
+    ; Borra el flag de direcci�n (Direction Flag).
+    cld 
+
+    ; DEC: Decrement by 1
+    ; Resta 1 al valor del operando.
+    mov eax, 10 
+    dec eax
+
+    ; IDIV: Signed Divide
+    ; Divide un n�mero entero con signo en el registro DX:AX por un operando de 8 bits o 16 bits.
+    mov eax, 20     
+    mov ebx, 4      
+    idiv ebx        
+
+    ; IMUL: Signed Multiply
+    ; Multiplica dos n�meros con signo y guarda el resultado en el registro de destino.
+    mov eax, 5      
+    imul ebx, 4     
+
+    ; POP: Pop a Value from the Stack
+    ; Saca un valor de la pila de la CPU y lo guarda en el operando.
+    pop ecx         
+
+    ; CMP: Compare Two Operands
+    ; Compara dos operandos y ajusta los flags de la CPU en consecuencia.
+    mov eax, 10   
+    cmp eax, 5      
+
+    ; LES: Load Far Pointer
+    ; Carga un puntero largo en un registro de segmento y un registro de desplazamiento.
+    les eax, [ebx] 
+
+    ; RCL: Rotate Through Carry Left
+    ; Rota los bits hacia la izquierda a trav�s del flag de acarreo (Carry Flag).
+    mov eax, 0xFF   
+    rcl eax, 1      
+
+    ; XCHG: Exchange Register/Memory with Register
+    ; Intercambia el contenido de un registro o una ubicaci�n de memoria con otro registro.
+    mov eax, 10     
+    mov ebx, 20     
+    xchg eax, ebx
+
+    ; JB: Jump if Below
+    ; Salta a la etiqueta especificada si el flag de acarreo (Carry Flag) est� activo.
+    mov eax, 5
+    mov ebx, 10
+    cmp eax, ebx
+    jb etiqueta  
+
+    ; JE: Jump if Equal
+    ; Salta a la etiqueta especificada si el flag de igualdad (Zero Flag) est� activo.
+    mov eax, 5
+    cmp eax, 5
+    je etiqueta   
+
+    ; JLE: Jump if Less or Equal
+    ; Salta a la etiqueta especificada si el flag de menos o igual (Less or Equal Flag) est� activo.
+    mov eax, 5
+    cmp eax, 10
+    jle etiqueta   
+
+    ; JNL: Jump if Not Less
+    ; Salta a la etiqueta especificada si el flag de no menos (Not Less Flag) est� activo.
+    mov eax, 5
+    cmp eax, 5
+    jnl etiqueta    
+
+    ; JNS: Jump if Not Sign
+    ; Salta a la etiqueta especificada si el flag de no signo (Not Sign Flag) est� activo.
+    mov eax, 5
+    cmp eax, 0
+    jns etiqueta   
+
+    ; JS: Jump if Sign
+    ; Salta a la etiqueta especificada si el flag de signo (Sign Flag) est� activo.
+    mov eax, -5
+    cmp eax, 0
+    js etiqueta 
+
+etiqueta:
+    ; C�digo adicional aqu�...
+
+section .data
+    ; Datos aqu�...
+
+section .bss
+    ; Variables no inicializadas aqu�... 
 ends
 
 
